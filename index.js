4a725fa79fc04a671ffb84c315535fb61000b4a9
yopaz-hieuvm <hieuvm@yopaz.dev>
2026-06-24 17:16:17 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello Node.js!'
+  });
+});
+
+app.get('/health', (req, res) => {
+  res.json({
+    status: 'OK'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
